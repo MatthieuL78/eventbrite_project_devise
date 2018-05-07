d4ed5ea4b8ad0f0e537629c41a7b5de401bea2aa
MatthieuL78 <matthieu.lu@outlook.fr>
2018-05-07 10:47:51 +0200

    Css and views first display

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :event_attendees
-  resources :events
+  root 'staticpages#home'
+  resources :events, :event_attendees
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
