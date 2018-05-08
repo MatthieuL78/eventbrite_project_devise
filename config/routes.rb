f034da668f1556360a35bcc6df91494ab6a6b275
MatthieuL78 <matthieu.lu@outlook.fr>
2018-05-08 20:30:06 +0200

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   post 'event_payement/:id', to: 'events#subscribe', as: :subscribe	
   get 'event_payement/:id', to: 'events#show_payement', as: :subscribe_show
   get 'event_payement/:id', to: 'events#create_payement', as: :payement_create
-  post 'create_payement/:id', to: 'events#payment_succeed', as: :payement_create
+  post 'create_payement/:id', to: 'events#payment_succeed', as: :payement_succeed
   resources :events
   devise_for :users
   resources :users, only: [:show]
